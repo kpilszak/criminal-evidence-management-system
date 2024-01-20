950207ca9770fb494d21649ecb76b96b99dafdfb
kpilszak <klaudia.pilszak@gmail.com>
2024-01-19 19:38:44 -0500

    Init repo

---
+++ src/test/java/com/kpilszak/criminalevidencemanagementsystem/CriminalEvidenceManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kpilszak.criminalevidencemanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CriminalEvidenceManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
