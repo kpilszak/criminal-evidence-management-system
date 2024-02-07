c1c20891af085beafbc1309b91bcdd8ed80eb396
kpilszak <klaudia.pilszak@gmail.com>
2024-02-07 08:59:49 -0500

    Prepare stub of AbstractRepo

--- src/test/java/com/kpilszak/criminalevidencemanagementsystem/repos/StubAbstractRepo.java
+++ src/test/java/com/kpilszak/criminalevidencemanagementsystem/repos/StubAbstractRepo.java
@@ -1,4 +1,31 @@
 package com.kpilszak.criminalevidencemanagementsystem.repos;
 
-public class StubAbstractRepo {
+import com.kpilszak.criminalevidencemanagementsystem.dao.AbstractEntity;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class StubAbstractRepo <T extends AbstractEntity> implements AbstractRepo<T> {
+
+    protected Map<Long, T> records = new HashMap<>();
+
+    @Override
+    public void save(T entity) {
+
+    }
+
+    @Override
+    public void delete(T entity) {
+
+    }
+
+    @Override
+    public int deleteById(Long entityId) {
+        return 0;
+    }
+
+    @Override
+    public T findById(Long entityId) {
+        return null;
+    }
 }
